18be3845ae065bc1531131f018ef7b6769c6d3c8
farzam seyedhashem <farzam.seyedhashem@gmail.com>
2024-01-08 15:20:41 +0330

    fefe

--- pages/api/send.js
+++ pages/api/send.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
         // })
         const {data, error} = await resend.emails.send({
             from: 'send@atighcompany.com',
-            to: ['sales@parspooyesh.com'],
+            to: ['sales@parspooyesh.com','farzam.seyedhashem@gmail.com'],
             // sales@parspooyesh.com
             subject: `${"درخواست دموی رایگان"+" "+reqData.name}`,
             react: EmailTemplate({reqData: reqData}),
