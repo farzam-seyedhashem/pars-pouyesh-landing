ba09f2561956998761e0c5e959334334a219bec2
farzam seyedhashem <farzam.seyedhashem@gmail.com>
2024-01-16 15:34:42 +0330

    fefe

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,25 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+        <head>
+            <script  async type={'text/javascript'} id={"google-tag-manager"} dangerouslySetInnerHTML={{__html:`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KPFFX88');`}}></script>
+            <title></title>
+            <meta name='robots' content='noindex'/>
+            <meta name={"title"} content={""}/>
+            <meta name="viewport" content="width=device-width"/>
+            <meta name="next-head-count" content="2"/>
+            <meta charSet="utf-8"/>
+            <Head/>
+        </head>
+
+
       <body>
         <Main />
         <NextScript />
--- pages/index.js
+++ pages/index.js
@@ -290,15 +290,12 @@ export default function Home() {
 
 
 
-            <Head>
-                <meta name='robots' content='noindex'/>
-            </Head>
+            {/*<Head>*/}
+
+            {/*    <meta name='robots' content='noindex'/>*/}
+            {/*</Head>*/}
             {/*Google Tag Manager*/}
-            <Script id={"google-tag-manager"} strategy={"beforeInteractive"} dangerouslySetInnerHTML={{__html:`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-KPFFX88');`}}/>
+
             {/*End Google Tag Manager*/}
 
 
