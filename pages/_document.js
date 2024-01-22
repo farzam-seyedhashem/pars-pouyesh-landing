af3747386b2daa7260ce3285ebd324ff79a8e608
farzam seyedhashem <farzam.seyedhashem@gmail.com>
2024-01-22 12:26:45 +0330

    fefe

--- pages/_document.js
+++ pages/_document.js
@@ -6,10 +6,10 @@ export default function Document() {
     <Html lang="en">
         <head>
             <script  async type={'text/javascript'} id={"google-tag-manager"} dangerouslySetInnerHTML={{__html:`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-KPFFX88');`}}></script>
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WMVT9J6J');`}}></script>
             <title></title>
             <meta name='robots' content='noindex'/>
             <meta name={"title"} content={""}/>
